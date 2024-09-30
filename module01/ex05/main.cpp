3de1dedc38ca7a667243f0bf55fc7607f63eac45
Matias Eduardo Isaguirre Escobar <misaguir@c2r5s4.42malaga.com>
2024-09-30 16:17:17 +0200

    cambiar mensajes del ex03 y el ex05 a ingles

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -1,5 +1,11 @@
-
+#include "Harl.hpp"
 int main (int argc, char **argv)
 {
-	
+	Harl level;
+	if (argc != 2)
+	{
+		std::cout << "Error arguments" << std::endl;
+		return (1);
+	}
+	level.complain(argv[1]);
 }
